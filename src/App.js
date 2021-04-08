021c0b9324344924d72155e47f8f2b86cb562579
coolboidmax <coolboidmax@gmail.com>
2021-04-08 10:52:33 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import background from './background.jpg';
 import './App.css';
+import Menu from './components/MenuComponent.js';
+import {AUTHORS} from './shared/authors';
 import {Navbar, NavbarBrand} from 'reactstrap';
 
-function App() {
-  return (
+
+
+class App extends Component  {
+  
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      
+      authors:AUTHORS
+      
+    };
+    
+    
+  }
+  
+  render(){
+    return (
     <div className="App">
     <Navbar dark color="primary">
     <div className="container">
+    <img src={background} className="rounded-circle img-top img-fluid pro" alt="logo" />
     <NavbarBrand href="\">iTech Liberia</NavbarBrand>
     </div>
     </Navbar>
+    
+        <Menu authors={this.state.authors} />
+    
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/shared/authors.js
@@ -0,0 +1,43 @@
+export const AUTHORS= [
+
+        {
+          id:0,
+          name: "Maxwell D. Dorliea",
+          image: "./profile.jpg",
+          title: "Senior Web Developer and Co-Founder",
+          Class: "Senior",
+          Servicepr: "1100.99",
+          hobby: "Programming and Music"
+        },
+        {
+          id:1,
+          name: "Prince Dahn",
+          image: "./profile.jpg",
+          title: "Senior Networker and Founder",
+          Class: "Senior",
+          Servicepr: "1100.99",
+          hobby: "Computer and Basketball"
+        },
+        
+        
+        
+        {
+          id:2,
+          name: "Stanley Garyeazohn",
+          image: "./profile.jpg",
+          title: "Senior Graphic Designer",
+          Class: "Senior",
+          Servicepr: "1100.99",
+          hobby: "Computer and Soccer"
+        },
+        
+        {
+          id:3,
+          name: "Adonis G.",
+          image: "./profile.jpg",
+          title: "Senior Networker and Co-Founded",
+          Class: "Senior",
+          Servicepr: "1100.99",
+          hobby: "Computer and Basketball"
+        }
+        ]
